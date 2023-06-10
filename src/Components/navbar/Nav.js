8a166e700fe9dfa0b086bd648bf67c8449043e18
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-06-10 11:33:18 +0530

    final commit

--- src/Components/navbar/Nav.js
+++ src/Components/navbar/Nav.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
 
 //import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 export default function () {
+ 
   return (
     <div>
       <nav>
       <h1>Touch Typing </h1>
       <div className="right-nav">
-        <button>submit your text</button>
-        <i class="fa fa-refresh fa-2x" aria-hidden="true"></i>
-     <h5><i class="fa fa-sign-in fa-2x" aria-hidden="true"></i> signIN</h5>
+        {/* <button>submit your text</button> */}
+        
       
       </div>
       </nav>
